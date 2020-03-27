948b562efa09068cb57edfc34d31f674ea9def36
Fynn Julius Withake <withake@cip.ifi.lmu.de>
2020-03-27 23:38:16 +0100

    deleted data folder

--- dwd_api.py
+++ dwd_api.py
@@ -11,3 +11,33 @@ station_info_files = {
     'hourly_temperature_historical': "TU_Stundenwerte_Beschreibung_Stationen.txt",
 }
 
+
+
+def parse_weather_station_info(text):
+    """This function extracts the weather station information from the text file provided by DWD
+    
+    Arguments:
+        text {String} -- the text retrieved vom the DWD Open Data server
+    
+    Returns:
+        df -- pandas dataframe containing weather station information
+    """
+    data = []
+    text = text.splitlines()
+    for line in text[2:]:
+        e = line.split()
+        station_id = e.pop(0)
+        start_date = e.pop(0)
+        end_date = e.pop(0)
+        altitude = e.pop(0)
+        latitude = e.pop(0)
+        longitude = e.pop(0)
+        state = e.pop(-1)
+        station_name = " ".join(e)
+
+        row = [station_id, start_date, end_date, altitude, latitude, longitude, station_name, state]
+        data.append(row)
+
+    columns = ["station_id", "start_date", "end_date", "altitude", "latitude", "longitude", "name", "state"]
+    df = pd.DataFrame(data, columns=columns)
+    return df
